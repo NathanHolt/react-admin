76ed1b381bb5f36121c21d21631f5bd08d99cda8
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2023-01-16 00:17:56 -0500

    added progress circle component

--- react-admin/src/scenes/dashboard/index.jsx
+++ react-admin/src/scenes/dashboard/index.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import Header from '../../components/Header'
+import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard = () => {
 
@@ -8,6 +9,7 @@ const Dashboard = () => {
             <Box display='flex' justifyContent='space-between' alignItems='center'>
                 <Header title='DASHBOARD' subtitle='Welcome to your Dashboard' />
             </Box>
+            <ProgressCircle />
         </Box>
     )
 }
